43902f411fbd70e72b824392672a88e0d2a9503b
Diana6777 <73775453+Diana6777@users.noreply.github.com>
2021-10-05 17:11:00 +0800

    Update Calc.java

--- src/main/java/com/daley/maven04/demo/Calc.java
+++ src/main/java/com/daley/maven04/demo/Calc.java
@@ -1,40 +1,13 @@
 package com.daley.maven04.demo;
-import java.util.Scanner;
+
+
 
 public class Calc {
-    public static int add(int a,int b){
-
-        return a+b;
-
-    }
-    public static int subtract( int a, int b){
-            return a + b;
-        }
-
-    public static void main(String args[]){
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("请输入第一个数");
-
-        int a = scanner.nextInt();
-
-        System.out.println("请输入第二个数");
-
-        int b = scanner.nextInt();
-
-        System.out.println("和为:"+add(a,b));
-        System.out.println("差为:"+subtract(a,b));
-
-    }
-//    public static void main(String[] args) {
-//
-//
-//    public add( int a, int b){
-//            return a + b;
-//        }
-//    public subtract( int a, int b){
-//            return a + b;
-//        }
+    public int add(int a, int b) {
+        return a + b;
     }
 
+    public int subtract(int a, int b) {
+        return a + b;
+    }
+}
